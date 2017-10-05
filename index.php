5c5aaad09a9fba85a657bd484e8b1526ba623cc2
Qntnroos <quinroos@student.arteveldehs.be>
2017-10-05 09:23:05 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php  
+        $naam = 'Quinten';
+    ?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Oefening 1 - Quinten Roos</title>
+</head>
+<body>
+   <h1>Hello <?= $naam ?></h1>
+</body>
+</html>
